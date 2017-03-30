b3eb53c070478fd8bb418e9d1e685d8b98a02c87
JonesRandom <jones.random97@gmail.com>
2017-03-30 22:34:41 +0800

    Request Permission

--- app/src/main/java/masxdeveloper/simplegalley/MainActivity.java
+++ app/src/main/java/masxdeveloper/simplegalley/MainActivity.java
@@ -1,12 +1,19 @@
 package masxdeveloper.simplegalley;
 
+import android.Manifest;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Environment;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -15,6 +22,7 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity implements LoaderPrensenter.OnFileLoaded {
 
     LoaderPrensenter presenter;
+    private static final int REQUEST_CODE = 0x00;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +30,6 @@ public class MainActivity extends AppCompatActivity implements LoaderPrensenter.
         setContentView(R.layout.activity_main);
 
         presenter = new LoaderPrensenter(this);
-        presenter.getListImage(Environment.getExternalStorageDirectory());
 
     }
 
@@ -44,5 +51,35 @@ public class MainActivity extends AppCompatActivity implements LoaderPrensenter.
         list.setLayoutManager(new GridLayoutManager(this, 3));
         list.setAdapter(adp);
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (PermissionAkses()) {
+            presenter.getListImage(Environment.getExternalStorageDirectory());
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, REQUEST_CODE);
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode) {
+            case REQUEST_CODE:
+
+                if (grantResults.length > 0 && grantResults[0] != PackageManager.PERMISSION_DENIED) {
+                    presenter.getListImage(Environment.getExternalStorageDirectory());
+                }
+                break;
+        }
+    }
+
+    private boolean PermissionAkses() {
+        return ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_DENIED;
+    }
+
 }
 
